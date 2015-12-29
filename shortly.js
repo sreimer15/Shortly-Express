c34672a3473707042bf5f1489274fe5027fb85a8
Sean Reimer <sreimer15@gmail.com>
2015-12-28 18:50:05 -0600

    Finishes the redirecting, and needs to be able to create user sessions.

--- shortly.js
+++ shortly.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
@@ -22,62 +24,115 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: true}
+  }));
+ 
+
+//We need to start a session for our user
+// finding a way to add keys to our req.session object
+  // check if logged in
+    // if logged in we can send them to the right pages
+    // else sign up?
+  //
 
 app.get('/', 
 function(req, res) {
-  res.render('index');
+
+  console.log("Session User: "+ Object.keys(req.session));
+  console.log("Session Error: "+req.session.error);
+  if(req.session.user) {
+    res.render('index');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get('/login',
+function(req,res){
+  res.render('login');
+});
+
+app.get('/signup',
+function(req, res) {
+  res.render('signup');
 });
 
 app.get('/create', 
 function(req, res) {
-  res.render('index');
+  if(req.session.user){
+    res.render('index');
+  }
+  else{
+    res.redirect('/login');
+  }
 });
 
 app.get('/links', 
 function(req, res) {
+if (req.session.user){
   Links.reset().fetch().then(function(links) {
     res.send(200, links.models);
-  });
+  });  
+} else {
+  res.redirect('/login');
+}
+
 });
 
 app.post('/links', 
 function(req, res) {
-  var uri = req.body.url;
+  if(req.session.user) {
+    var uri = req.body.url;
 
-  if (!util.isValidUrl(uri)) {
-    console.log('Not a valid url: ', uri);
-    return res.send(404);
-  }
-
-  new Link({ url: uri }).fetch().then(function(found) {
-    if (found) {
-      res.send(200, found.attributes);
-    } else {
-      util.getUrlTitle(uri, function(err, title) {
-        if (err) {
-          console.log('Error reading URL heading: ', err);
-          return res.send(404);
-        }
-
-        var link = new Link({
-          url: uri,
-          title: title,
-          base_url: req.headers.origin
-        });
-
-        link.save().then(function(newLink) {
-          Links.add(newLink);
-          res.send(200, newLink);
-        });
-      });
+    if (!util.isValidUrl(uri)) {
+      console.log('Not a valid url: ', uri);
+      return res.send(404);
     }
-  });
+
+    new Link({ url: uri }).fetch().then(function(found) {
+      if (found) {
+        res.send(200, found.attributes);
+      } else {
+        util.getUrlTitle(uri, function(err, title) {
+          if (err) {
+            console.log('Error reading URL heading: ', err);
+            return res.send(404);
+          }
+
+          var link = new Link({
+            url: uri,
+            title: title,
+            base_url: req.headers.origin
+          });
+
+          link.save().then(function(newLink) {
+            Links.add(newLink);
+            res.send(200, newLink);
+          });
+        });
+      }
+    });
+  } else {
+  res.redirect('/login');
+  }
 });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
 
+app.post('/signup', function(req,res){
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  // Use bookshelf to create a user and create a session
+
+});
 
 
 /************************************************************/
